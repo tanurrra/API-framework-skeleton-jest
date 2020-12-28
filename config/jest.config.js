93a9d5d7b8e9477cc5a640f6743f35cf1e602f91
tanurrra <22240559+tanurrra@users.noreply.github.com>
2020-12-29 00:12:35 +0100

    fix rootDir config

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = async () => {
     return {
         verbose: true,
-        rootDir: "../API-jest",
+        rootDir: "../",
         reporters: [
             "default"
         ],
